b4833b0c7b1e3961981eb9cadcb5d155867b4541
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-07 08:58:54 -0700

    changesss

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', function (req,res) {
     res.send("Hello World")
 })
