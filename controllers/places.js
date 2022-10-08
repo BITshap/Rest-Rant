e0fdc67496b04becc9e7d28ffe49c7a4aac8b164
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-08 09:29:18 -0700

    renderingPlaces

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index', {places})
 })
 
 module.exports = router
