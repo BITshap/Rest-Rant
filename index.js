2a35acdb35e47145e4dd62c249999d717c94c04f
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-08 09:01:41 -0700

    fixed

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// MIDDLEWARE
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
+console.log("I am running!")
+
--- views/default.jsx
+++ views/default.jsx
@@ -11,6 +11,7 @@ function Def (html) {
             </body>
         </html>
     )
-}
+  }
+  
 
 module.exports = Def
