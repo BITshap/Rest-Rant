5eb44613fc629650e2ae263df30fdf697749aa24
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-10 14:00:47 -0700

    WorkingEdit

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,29 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      if (!req.body.pic) {
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
@@ -20,6 +43,26 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    res.render('places/edit', {place: places[id], id})
+  }
+})
+
+
+
+
+
+
+
+
+
+
 router.post('/', (req, res) => {
   if(!req.body.pic) {
     req.body.pic = 'http://placekitten.com/400/400'
@@ -33,6 +76,9 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+
+
 
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
