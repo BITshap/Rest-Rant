59ff4c2f38c3c1f6986c81d89ff060dd170ec5be
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-19 17:44:10 -0700

    validation-backend

--- models/places.js
+++ models/places.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: {type: String, required: true},
-  pic: String,
+  pic: {type: String, default: 'http://placekitten.come/350/350'},
   cuisines: {type: String, required: true},
   city: {type: String, default: 'Anytown'},
   state: {type: String, default: 'USA'},
-  founded: Number
+  founded: {
+    type: Number,
+    min: [1673, 'Surely not that old?!'],
+    max: [new Date().getFullYear(), 'Hey, this year is in the future!']
+  }
 })
+
 placeSchema.methods.showEstablished = function(){
   return `${this.name} had been serving ${this.city}, ${this.state} since ${this.founded}`
 }
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -30,7 +30,7 @@ function new_form (){
                     </div>
                     <div className="form-group">
                         <label htmlFor="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded"/>
+                        <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()}/>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place"/>
                 </form>
